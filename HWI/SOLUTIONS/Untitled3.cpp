5b1598d39fce6695cd0d44b858fd6ce7737d18e7
Kritansh Mehrotra <kritanshmehrotra2@gmail.com>
2020-08-04 23:52:55 +0530

    Removing Unnecessary Files

--- HWI/SOLUTIONS/Untitled3.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-main()
-int a[]={1,2,3}
-{
-a++;
-printf("%d", *a)
-}
--- HWI/SOLUTIONS/eeee.c
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#define ll long long
-using namespace std;
-int main()
-{
-	ll t{0},a{0},b{0},c{0},i{0},j{0};
-
-	return 0;
-}
